a1f55b4d461e70e76e03abfcb43653840be2387e
Arturo Torres <dvluistorres@gmail.com>
2022-12-17 19:41:31 -0500

    Add task modal html, css and js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import './style.css';
-import {footer} from './devFooter'
+import {footer} from './devFooter';
+
 
 footer.add();
 
 const project = (()=>{
-    const createProject = (name , importance , dueDate) => {
-        return{name , importance , dueDate}
+    const projectFactory = (name , importance , dueDate) => {
+        let position = 0;
+        let tasks = [1,2,3,4,5,6,7,8,9,10];
+        let completedTasks = 0;
+        return{name , importance , dueDate , position , tasks , completedTasks}
     }
+
+    let myProjects = []
+
+    const retrieveMyProjects = () =>{
+        try {
+            myProjects = JSON.parse(localStorage.getItem('myProjects'));
+            modifyDOM.addSidebarProject(myProjects);
+            console.log("retrieved")
+        } catch (error) {
+            console.log("No projects in local storage yet");
+            myProjects = []
+        }
+    }
+
+    function addProject(){
+        const projectName = document.getElementById('projectName').value;
+        const projectImportance = document.querySelector('input[name="ProjectImportance"]:checked').value;
+        const dueDate = document.getElementById('dueDate').value;
+        const newProject = projectFactory(projectName,projectImportance, dueDate);
+        newProject.numberOfTasks = 8;
+        newProject.completedTasks = 4;
+        document.getElementById('projectName').value = ""
+        document.getElementById('dueDate').value = ""
+        myProjects.push(newProject);
+        myProjects.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
+        let positionCounter = 0
+        for (const project of myProjects){
+            project.position = positionCounter;
+            positionCounter++;
+        }
+        modifyDOM.addSidebarProject(myProjects);
+        modal.createProject();
+        localStorage.setItem('myProjects', JSON.stringify(myProjects));
+    }
+
+    function addTask(){
+        console.log(1)
+    }
+
+    return {addProject , retrieveMyProjects , addTask}
 })()
 
 const modal = (()=>{
@@ -21,11 +65,101 @@ const modal = (()=>{
           bodyClassList.add("projectModal");
         }
       };
-      document.getElementById("createProject").innerHTML ='<fieldset id="projectData" class="projectData"><p>Add project</p><div><label for="name">Name:</label><input type="text" id="name" placeholder="Your project name here" name="name" required></div>          <div>              <label for="dueDate">Due date?:</label>              <input type="date" value="dueDate" id="dueDate" name="dueDate" required>          </div>          <div>              <label id="ProjectImportance" for="ProjectImportance">Project importance?:</label>              <div class="radioWrapper" style="display:flex;gap:12px">                  <div>                      <input type="radio" id="important" name="ProjectImportance" value="important">                      <label for="important">important</label>                  </div>                                    <div>                      <input type="radio" id="normal" name="ProjectImportance" value="normal" checked>                      <label for="normal">normal</label>                  </div>                                    <div>                      <input type="radio" id="nonImportant" name="ProjectImportance" value="nonImportant">                      <label for="nonImportant">Non important</label>                  </div>              </div>          </div>                    <button class="projectCreate modalButton"><b>Add Project</b></button>          <button class="modalCreateProject modalButton" >Cancel</button>   </fieldset>'
-    return {createProject}
+
+    const createTask = () => {
+    const bodyClassList = document.body.classList;
+
+    if (bodyClassList.contains("taskModal")) {
+        bodyClassList.remove("taskModal");
+        bodyClassList.add("open");
+    } else {
+        bodyClassList.remove("open");
+        bodyClassList.add("taskModal");
+    }
+    };
+
+    return {createProject,createTask}
+})()
+
+const modifyDOM = (()=>{
+    const addSidebarProject = (projects) => {
+        removeListenersByClass("sidebarCard" , sidebarCardClick);
+        removeElementsByClass("sidebarCard");
+        for (const project of projects){
+            const addProgress = ({
+                progress: () => Math.round(project.completedTasks / project.tasks.length * 100)
+            });
+            Object.assign(project,addProgress);
+            const sidebarCard = createElement('div',`sidebarCard ${project.position}`);
+            const cardTitle = createElement ('p','cardTitle');
+            cardTitle.innerText = project.name;
+            const cardProgressBar = createElement('div','cardProgressBar');
+            const cardProgress = createElement('div','cardProgress');
+            cardProgress.style.width = `${project.progress()}%`;
+            const importanceColor = () => {
+                if (project.importance === 'important'){
+                    return 'red'
+                } else if (project.importance === 'normal'){
+                    return 'yellow'
+                } else {
+                    return 'green'
+                }
+            }
+            sidebarCard.style.borderBottom = `solid 3px ${importanceColor()}`;
+            cardProgressBar.append(cardProgress);
+            sidebarCard.append(cardTitle , cardProgressBar);
+            document.getElementById('projects').appendChild(sidebarCard);
+            sidebarCard.addEventListener('click',sidebarCardClick);
+            const opt = document.createElement('option');
+            opt.setAttribute("class","projectSelectionList")
+            opt.value = project.name;
+            opt.textContent = project.name;
+            document.getElementById('parentProject').appendChild(opt);
+        }
+        
+
+    };
+
+    function sidebarCardClick (){
+        console.log("1")
+    }
+
+    function removeElementsByClass(className){
+        const elements = document.getElementsByClassName(className);
+        while(elements.length > 0){
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    }
+
+    function removeListenersByClass(className,listenerFunction){
+    const elements = document.getElementsByClassName(className);
+    for (const element of elements){
+        element.removeEventListener('click',listenerFunction);
+    }
+    }
+
+    function createElement (type , className){
+        const newElement = document.createElement(type);
+        newElement.setAttribute('class',className);
+        return newElement
+    }
+    return {addSidebarProject , createElement}
 })()
 
 const modalCreateProject = document.getElementsByClassName('modalCreateProject');
 for (const element of modalCreateProject){
     element.addEventListener('click',modal.createProject)
 }
+
+const modalCreateTask = document.getElementsByClassName('modalCreateTask');
+for (const element of modalCreateTask){
+    element.addEventListener('click',modal.createTask)
+}
+
+project.retrieveMyProjects();
+
+const projectCreateButton = document.getElementById('projectCreateButton');
+projectCreateButton.addEventListener('click',project.addProject);
+
+const taskCreateButton = document.getElementById('taskCreateButton');
+taskCreateButton.addEventListener('click',project.addTask);
