8d3dbbd3cbac7569996388305ce27d107556214a
Arturo Torres <dvluistorres@gmail.com>
2022-12-20 18:46:31 -0500

    Style cards, Add content to right panel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './style.css';
 import {footer} from './devFooter';
-import { compareAsc, parseISO } from 'date-fns'
+import { compareAsc, format, parseISO } from 'date-fns'
 
 
 footer.add();
 
 const project = (()=>{
-    const projectFactory = (name , importance , dueDate) => {
+    const _projectFactory = (name , importance , dueDate) => {
         let position = 0;
         let tasks = [];
         let completedTasks = 0;
@@ -14,7 +14,7 @@ const project = (()=>{
         return{name , importance , dueDate , position , tasks , completedTasks , progress}
     }
 
-    const taskFactory = (name,dueDate,importance,notes,parentProject)=>{
+    const _taskFactory = (name,dueDate,importance,notes,parentProject)=>{
         let position = 0;
         let isCompleted = false;
         return{name,dueDate,importance,notes,parentProject,position,isCompleted}
@@ -26,11 +26,13 @@ const project = (()=>{
         try {
             myProjects = JSON.parse(localStorage.getItem('myProjects'));
             modifyDOM.addSidebarProject(myProjects);
-            console.log("retrieved")
         } catch (error) {
             console.log("No projects in local storage yet");
-            myProjects = []
         }
+    }
+
+    function saveToLocal(){
+        localStorage.setItem('myProjects', JSON.stringify(myProjects));
     }
 
     function addProject(){
@@ -45,8 +47,11 @@ const project = (()=>{
         if (compareAsc(parseISO(dueDate),new Date()) < 0){
             alert("Due date already passed");
             return
+        } else if (dueDate == ''){
+            alert('Please insert due date');
+            return
         }
-        const newProject = projectFactory(projectName,projectImportance, dueDate);
+        const newProject = _projectFactory(projectName,projectImportance, dueDate);
         document.getElementById('projectName').value = '';
         document.getElementById('dueDate').value = '';
         myProjects.push(newProject);
@@ -58,7 +63,7 @@ const project = (()=>{
         }
         modifyDOM.addSidebarProject(myProjects);
         modal.createProject();
-        localStorage.setItem('myProjects', JSON.stringify(myProjects));
+        saveToLocal();
     }
 
     function addTask(){
@@ -67,7 +72,7 @@ const project = (()=>{
         const taskImportance = document.querySelector('input[name="taskImportance"]:checked').value;
         const taskNotes = document.getElementById('taskNotes').value;
         const parentProject = document.getElementById('parentProject').value;
-        const newTask = taskFactory(taskName,taskDueDate,taskImportance,taskNotes,parentProject)
+        const newTask = _taskFactory(taskName,taskDueDate,taskImportance,taskNotes,parentProject)
         document.getElementById('taskName').value = '';
         document.getElementById('taskDueDate').value = '';
         document.getElementById('taskNotes').value = '';
@@ -80,8 +85,10 @@ const project = (()=>{
             positionCounter++;
         }
         modal.createTask();
-        localStorage.setItem('myProjects', JSON.stringify(myProjects));
+        saveToLocal();
         modifyDOM.addSidebarProject(myProjects);//to update bar progress
+        modifyDOM.updateExplorer(myProjects.findIndex(project => project.name === parentProject))
+
     }
 
     function clearProjects(){
@@ -97,7 +104,12 @@ const project = (()=>{
         return myProjects
     }
 
-    return {retrieveMyProjects , addProject , addTask , clearProjects , getMyProjects}
+    function updateCompletedTasks(position){
+        const completed = myProjects[position].tasks.filter(task => task.isCompleted).length
+        myProjects[position].completedTasks = completed
+    }
+
+    return {retrieveMyProjects , addProject , addTask , clearProjects , getMyProjects , updateCompletedTasks , saveToLocal}
 })()
 
 const modal = (()=>{
@@ -130,9 +142,9 @@ const modal = (()=>{
 
 const modifyDOM = (()=>{
     const addSidebarProject = (projects) => {
-        removeListenersByClass("sidebarCard" , sidebarCardClick);
-        removeElementsByClass("sidebarCard");
-        removeElementsByClass("projectSelectionList");
+        _removeListenersByClass("sidebarCard" ,'click', _sidebarCardClick);
+        _removeElementsByClass("sidebarCard");
+        _removeElementsByClass("projectSelectionList");
         for (const project of projects){
             const addProgress = ({
                 setProgress: () => {
@@ -147,21 +159,17 @@ const modifyDOM = (()=>{
             const cardProgressBar = createElement('div','cardProgressBar');
             const cardProgress = createElement('div','cardProgress');
             project.setProgress();
-            cardProgress.style.width = `${project.progress}%`;
-            const importanceColor = () => {
-                if (project.importance === 'important'){
-                    return 'red'
-                } else if (project.importance === 'normal'){
-                    return 'yellow'
-                } else {
-                    return 'green'
-                }
+            if (project.progress === 100 || isNaN(project.progress)){
+                cardProgress.setAttribute('style','clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%)');
+            } else {
+                cardProgress.setAttribute('style','clip-path: polygon(0 0, 100% 0, 95% 100%, 0% 100%)');
             }
-            sidebarCard.style.borderBottom = `solid 3px ${importanceColor()}`;
+            cardProgress.style.width = `${project.progress}%`;
+            sidebarCard.style.borderLeft = `solid 5px ${_importanceColor(project)}`;
             cardProgressBar.append(cardProgress);
             sidebarCard.append(cardTitle , cardProgressBar);
             document.getElementById('projects').appendChild(sidebarCard);
-            sidebarCard.addEventListener('click',sidebarCardClick);
+            sidebarCard.addEventListener('click',_sidebarCardClick);
             const opt = document.createElement('option');
             opt.setAttribute("class","projectSelectionList")
             opt.value = project.name;
@@ -172,28 +180,93 @@ const modifyDOM = (()=>{
 
     };
 
-    function sidebarCardClick (){
-        removeListenersByClass("explorerCard" , explorerCardClick);
-        removeElementsByClass("explorerCard");
-        const currentProject = project.getMyProjects()[event.currentTarget.classList[1]];
-        console.log(currentProject)
+    const _importanceColor = (currentProject) => {
+        if (currentProject.isCompleted){
+            return 'green'
+        } else if (currentProject.importance === 'important'){
+            return 'red'
+        } else if (currentProject.importance === 'normal'){
+            return 'orange'
+        } else {
+            return 'yellow'
+        }
     }
 
-    function explorerCardClick (){
+    function updateExplorer(projectPosition){
+        //Create Title
+        const explorer = document.getElementById('explorer');
+        _removeListenersByClass("explorerCard" ,'click' , _explorerCardTitleClick);
+        _removeElementsByClass("explorerCard");
+        _removeElementsByClass("taskCard");
+        const currentProject = project.getMyProjects()[projectPosition];
+        const explorerTitle = createElement('div','explorerCard explorerTittle');
+        const cardTitle = createElement ('p','cardTitle');
+        cardTitle.innerText = currentProject.name;
+        const cardDate = createElement ('span','cardDate');
+        cardDate.innerText = currentProject.dueDate;
+        const cardProgressBar = createElement('div','cardProgressBar');
+        const cardProgress = createElement('div','cardProgress');
+        currentProject.setProgress();
+        cardProgress.style.width = `${currentProject.progress}%`;
+        explorerTitle.style.borderBottom = `solid 3px ${_importanceColor(currentProject)}`;
+        cardProgressBar.append(cardProgress);
+        explorerTitle.append(cardTitle , cardDate , cardProgressBar);
+        explorer.appendChild(explorerTitle);
+        //Crate task cards
+        for (const task of currentProject.tasks){
+            const taskCard = createElement('div','taskCard');
+            const cardCheckbox = createElement('input','cardCheckbox');
+            cardCheckbox.setAttribute('type','checkbox');
+            cardCheckbox.checked = task.isCompleted;
+            const updateCompletion = ({
+                updateCompletion: () => {
+                    task.isCompleted = !task.isCompleted
+                }
+            });
+            Object.assign(task,updateCompletion);
+            cardCheckbox.addEventListener('change', function cardCheckboxClick(Event){
+                task.updateCompletion();
+                project.updateCompletedTasks(projectPosition);
+                currentProject.setProgress();
+                document.getElementsByClassName('cardProgress')[projectPosition].setAttribute('style',`width: ${currentProject.progress}%`);
+                cardProgress.style.width = `${currentProject.progress}%`;
+                project.saveToLocal();
+            })
+            const cardTitle = createElement('div','taskTitle');
+            cardTitle.innerText = task.name;
+            const cardDate = createElement('div','taskDate');
+            cardDate.innerText = format(parseISO(task.dueDate),'dd/MM/yyyy');
+            const cardImportance = createElement('div','taskImportance');
+            cardImportance.style.backgroundColor = _importanceColor(task);
+            taskCard.append(cardCheckbox,cardTitle,cardDate,cardImportance);
+            explorer.appendChild(taskCard);
+        }
+    }
+
+    function _sidebarCardClick (){
+        const clickedProjectPosition = () => {
+            if (event.currentTarget.classList[0] === 'sidebarCard'){
+                return event.currentTarget.classList[1]
+            }}
+
+        updateExplorer(clickedProjectPosition()); 
+        }
+
+    function _explorerCardTitleClick (){
         console.log('1')
     }
 
-    function removeElementsByClass(className){
+    function _removeElementsByClass(className){
         const elements = document.getElementsByClassName(className);
         while(elements.length > 0){
             elements[0].parentNode.removeChild(elements[0]);
         }
     }
 
-    function removeListenersByClass(className,listenerFunction){
+    function _removeListenersByClass(className,type,listenerFunction){
     const elements = document.getElementsByClassName(className);
     for (const element of elements){
-        element.removeEventListener('click',listenerFunction);
+        element.removeEventListener(type,listenerFunction);
     }
     }
 
@@ -202,7 +275,7 @@ const modifyDOM = (()=>{
         newElement.setAttribute('class',className);
         return newElement
     }
-    return {addSidebarProject , createElement}
+    return {addSidebarProject , createElement , updateExplorer}
 })()
 
 const modalCreateProject = document.getElementsByClassName('modalCreateProject');
