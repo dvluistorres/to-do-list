a5f026a11e656727fe5ef873a5ff3ea0fac2e3d8
Arturo Torres <dvluistorres@gmail.com>
2023-02-25 23:55:21 -0500

    Add button fix

--- src/devFooter.js
+++ src/devFooter.js
@@ -1,18 +1,18 @@
-import gitLogo from "./images/github.png";
-import "./devFooterStyle.css"
+import gitLogo from './images/github.png';
+import './devFooterStyle.css';
 
-const footer = (() =>{
-    const add = ()=>{
-        const footerDiv = document.getElementById('footer');
-        footerDiv.innerText='Copyright © 2021 devLuisTorres';
-        const githubLogo = new Image();
-        githubLogo.src = gitLogo;
-        footerDiv.appendChild(githubLogo);
-        githubLogo.addEventListener('click', () => {
-            window.open('https://github.com/dvluistorres', '_blank');
-            });
-    }
-    return {add}
-})()
+const footer = (() => {
+  const add = () => {
+    const footerDiv = document.getElementById('footer');
+    footerDiv.innerText = 'Copyright © 2021 devLuisTorres';
+    const githubLogo = new Image();
+    githubLogo.src = gitLogo;
+    footerDiv.appendChild(githubLogo);
+    githubLogo.addEventListener('click', () => {
+      window.open('https://github.com/dvluistorres', '_blank');
+    });
+  };
+  return { add };
+})();
 
-export {footer}
+export { footer };
